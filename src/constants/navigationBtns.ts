722229f104bdbf49900440336f07f04aa93b0c53
Dake69 <brawlplayer914@gmail.com>
2023-05-07 12:40:20 +0300

    1

--- src/constants/navigationBtns.ts
+++ src/constants/navigationBtns.ts
@@ -11,5 +11,229 @@ export const navBar = [
         label: 'Герої',
         link: '/basics/heroes'
     },
+    {
+        id: 1.2,
+        label: 'Предмети',
+        link: '/basics/items'
+    },
+    {
+        id: 1.3,
+        label: 'Основи',
+        link: '/basics/lanesCreepsTowers'
+    },
+    {
+        id: 2,
+        label: 'Механіка',
+        link: '/mechanics'
+    },
+    {
+        id: 2.1,
+        label: 'Механіка',
+        link: '/mechanics/unitLVL'
+    },
+    {
+        id: 2.2,
+        label: 'Механіка',
+        link: '/mechanics/goldBounties'
+    },
+    {
+        id: 2.3,
+        label: 'Механіка',
+        link: '/mechanics/experience'
+    },
+    {
+        id: 2.4,
+        label: 'Механіка',
+        link: '/mechanics/lastHitsAndDenying'
+    },
+    {
+        id: 2.5,
+        label: 'Механіка',
+        link: '/mechanics/damage'
+    },
+    {
+        id: 2.6,
+        label: 'Механіка',
+        link: '/mechanics/death'
+    },
+    {
+        id: 2.7,
+        label: 'Механіка',
+        link: '/mechanics/unitStats'
+    },
+    {
+        id: 2.8,
+        label: 'Механіка',
+        link: '/mechanics/abilities'
+    },
+    {
+        id: 2.9,
+        label: 'Механіка',
+        link: '/mechanics/statusEffects'
+    },
+    {
+        id: 3,
+        label: 'Механіка',
+        link: '/units'
+    },
+    {
+        id: 3.1,
+        label: 'Механіка',
+        link: '/units/structures'
+    },
+    {
+        id: 3.2,
+        label: 'Механіка',
+        link: '/units/lineCreeps'
+    },
+    {
+        id: 3.3,
+        label: 'Механіка',
+        link: '/units/smallNeutralCamps'
+    },
+    {
+        id: 3.4,
+        label: 'Механіка',
+        link: '/units/bigNeutralCamps'
+    },
+    {
+        id: 3.5,
+        label: 'Механіка',
+        link: '/units/ancientNeutralCamps'
+    },
+    {
+        id: 4,
+        label: 'Механіка',
+        link: '/items'
+    },
+    {
+        id: 4.1,
+        label: 'Механіка',
+        link: '/items/shopItems'
+    },
+    {
+        id: 4.2,
+        label: 'Механіка',
+        link: '/items/neutralItems'
+    },
+    {
+        id: 4.3,
+        label: 'Механіка',
+        link: '/items/inventory'
+    },
+    {
+        id: 4.4,
+        label: 'Механіка',
+        link: '/items/recipesAssembly'
+    },
+    {
+        id: 4.5,
+        label: 'Механіка',
+        link: '/items/courier'
+    },
+    {
+        id: 5,
+        label: 'Механіка',
+        link: '/theBattlefield'
+    },
+    {
+        id: 5.1,
+        label: 'Механіка',
+        link: '/theBattlefield/minimap'
+    },
+    {
+        id: 5.2,
+        label: 'Механіка',
+        link: '/theBattlefield/lanes'
+    },
+    {
+        id: 5.3,
+        label: 'Механіка',
+        link: '/theBattlefield/shops'
+    },
+    {
+        id: 5.4,
+        label: 'Механіка',
+        link: '/theBattlefield/structures'
+    },
+    {
+        id: 5.5,
+        label: 'Механіка',
+        link: '/theBattlefield/vision'
+    },
+    {
+        id: 5.6,
+        label: 'Механіка',
+        link: '/theBattlefield/runes'
+    },
+    {
+        id: 5.7,
+        label: 'Механіка',
+        link: '/theBattlefield/roshan'
+    },
+    {
+        id: 5.8,
+        label: 'Механіка',
+        link: '/theBattlefield/tormentor'
+    },
+    {
+        id: 6,
+        label: 'Механіка',
+        link: '/teamWork'
+    },
+    {
+        id: 6.1,
+        label: 'Механіка',
+        link: '/teamWork/communication'
+    },
+    {
+        id: 6.2,
+        label: 'Механіка',
+        link: '/teamWork/terminology'
+    },
+    {
+        id: 6.3,
+        label: 'Механіка',
+        link: '/teamWork/laneEquilibrium'
+    },
+    {
+        id: 6.4,
+        label: 'Механіка',
+        link: '/teamWork/teamFights'
+    },
+    {
+        id: 6.5,
+        label: 'Механіка',
+        link: '/teamWork/objectives'
+    },
+    {
+        id: 6.6,
+        label: 'Механіка',
+        link: '/teamWork/heroItemChoice'
+    },
+    {
+        id: 7,
+        label: 'Механіка',
+        link: '/advanced'
+    },
+    {
+        id: 7,
+        label: 'Механіка',
+        link: '/advanced/commonBufs'
+    },
+    {
+        id: 7,
+        label: 'Механіка',
+        link: '/advanced/abilityTargeting'
+    },
+    {
+        id: 7,
+        label: 'Механіка',
+        link: '/advanced/unitOrders'
+    },
+    {
+        id: 7,
+        label: 'Механіка',
+        link: '/advanced/unitAggression'
+    },
 ];
-
